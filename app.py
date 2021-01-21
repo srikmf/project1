1fe6ccfaef16341208afa0a817072d814cf73a44
srikanth <srikmf@gmail.com>
2021-01-21 19:08:45 +0530

    done

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import numpy as np
 
 
 
-application = Flask(__name__) # initializing a flask app
+app = Flask(__name__) # initializing a flask app
 # app=application
-@application.route('/',methods=['GET'])  # route to display the home page
+@app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@application.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
 @cross_origin()
 def index():
     if request.method == 'POST':
@@ -59,5 +59,5 @@ def index():
         return render_template('index.html')
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	application.run(debug=True) 
+	app.run(debug=True)
     # running the app
